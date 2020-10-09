823b300e07f0a0c1d7f7ef31d772e88757f0151e
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-10-09 15:55:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function OnClick()
 {
-    document.getElementById("div").textContent = inp.value;
+    document.getElementById("div").textContent = three(document.getElementById ("inp").value);
+}
+function three(str)//прост захотілось
+{
+    return str[0].toUpperCase()+str.slice(1);
 }
